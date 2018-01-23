ecd128d2d3338e4de18f4124edd321cd36868c12
dajich86 <35721780+dajich86@users.noreply.github.com>
2018-01-23 06:14:35 -0600

    Update WebServer con 2 salidas + WifiManager_credenciales.ino

--- WebServer con 2 salidas + WifiManager_credenciales.ino
+++ WebServer con 2 salidas + WifiManager_credenciales.ino
@@ -1,16 +1,7 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <DNSServer.h>
-#include <WiFiManager.h>  
-
-MDNSResponder mdns;
-
-// Replace with your network credentials
-//const char* ssid = "Nito";
-//const char* password = "doremifa";
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 ESP8266WebServer server(80);
 
@@ -48,39 +39,16 @@ webPage += "    <h4 class='text-center'>www.reefacuario.com</h4>";
 webPage += "</div>";
 webPage += "</body>";
 webPage += "</html>";
-
-
-
-  //webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
-  //webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
   
   // preparing GPIOs
   pinMode(gpio0_pin, OUTPUT);
   digitalWrite(gpio0_pin, LOW);
   pinMode(gpio2_pin, OUTPUT);
   digitalWrite(gpio2_pin, LOW);
-  
-  delay(1000);
-  Serial.begin(115200);
-  //WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  //Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  delay(500);
   
-  if (mdns.begin("esp8266", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
-  }
-  
-  server.on("/", [](){
+   server.on("/", [](){
     server.send(200, "text/html", webPage);
   });
   server.on("/socket1On", [](){
@@ -104,7 +72,6 @@ webPage += "</html>";
     delay(1000); 
   });
   server.begin();
-  Serial.println("HTTP server started");
 }
  
 void loop(void){
