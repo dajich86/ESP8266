8c98d1edf9cf62e3c6d334976b3805ee629154a7
dajich86 <35721780+dajich86@users.noreply.github.com>
2020-08-24 14:58:28 -0500

    Create Blink.ino

---
+++ Blink.ino
@@ -0,0 +1,14 @@
+//Programa de prueba del led integrado
+#define LED_BUILTIN 2
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+}
